5c8bcfb38be4637cee7ae69075643776cacdf394
nicolasluarte <l.luarte@uc.cl>
2021-08-01 15:56:24 -0400

    week 1 intake plot

--- scripts/pilot.R
+++ scripts/pilot.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 library(ggplot2)
+library(gridExtra)
+library(cowplot)
 library(timetk)
 library(ggpubr)
 
 # load data -------------------------------------------------------------------
 
-list.files(path = "../data", full.names = TRUE) %>%
+list.files(path = "../data/week_1", full.names = TRUE) %>%
 	map_dfr(~read_csv(.)) -> raw_data
 
 # preprocess ------------------------------------------------------------------
@@ -17,27 +19,70 @@ raw_data %>%
 
 # fix pellet count
 
-data %>%
-	group_by(Mouse) %>%
-	mutate(PelletCount = seq(1, n())) -> data
+# data %>%
+# 	group_by(Mouse) %>%
+# 	mutate(PelletCount = seq(1, n())) -> data
 
 # add pellet count
 data %>%
 	mutate(freq = 1) -> data
 
-# add light/dark
+# add day
+data %>%
+	mutate(day = lubridate::day(date)) -> data
 
+# add hour minute second
+data %>%
+	mutate(hour = lubridate::hour(date) +
+			lubridate::minute(date) / 60 +
+			lubridate::second(date) / 3600
+		) -> data
+
+# add light/dark
 data %>%
 	mutate(lights = if_else(
-				lubridate::hour(date) >= 16 | lubridate::hour(date) <= 6,
+				hour >= 19 | hour <= 7,
 				"dark",
 				"light"
 				)) -> data
 
-# add day
+# add hour bins
 data %>%
-	mutate(hour = lubridate::hour(date)) %>%
-	mutate(day = lubridate::day(date)) -> data
+	mutate(hour_bin = cut(hour, 24)) -> data
+
+# pellet sum per bin
+data %>%
+	filter(day %in% c(27, 28, 29)) %>%
+	group_by(hour_bin, day) %>%
+	summarise(pellet_intake = sum(freq)) %>%
+	ungroup() %>%
+	group_by(hour_bin) %>%
+	summarise(pellet_mean = mean(pellet_intake), pellet_sem = sd(pellet_intake) / sqrt(n())) %>%
+	mutate(hour_bin = as.numeric(hour_bin)) %>%
+	ungroup() -> pellet_intake
+
+# pellet light/dark
+data %>%
+	filter(day %in% c(27, 28, 29)) %>%
+	group_by(lights, day) %>%
+	summarise(pellet_intake = sum(freq)) %>%
+	ungroup() %>%
+	group_by(lights) %>%
+	summarise(pellet_mean = mean(pellet_intake), pellet_sem = sd(pellet_intake) / sqrt(n()))
+
+data %>%
+	filter(hour_bin != 12, hour_bin != 13) %>%
+	filter(day %in% c(27, 28, 29)) %>%
+	group_by(hour_bin, lights) %>%
+	summarise(pellet_intake = sum(freq)) 
+
+	ungroup() %>%
+	group_by(lights) %>%
+	summarise(pellet_mean = mean(pellet_intake))
+
+-> diff_intake
+
+t.test(pellet_intake ~ lights, paired = TRUE, data = diff_intake)
 
 # add inter removal interval
 data %>%
@@ -62,6 +107,29 @@ data %>%
 
 
 # plots -----------------------------------------------------------------------
+
+data %>%
+	filter(hour > 13 | hour < 12) %>%
+	ggplot(aes(hour, as.factor(freq))) +
+	geom_point(pch = "|") +
+	scale_x_continuous(breaks = seq(0, 24)) +
+	theme_pubr() +
+	coord_fixed(ratio = 0.5) +
+	ylab("") +
+	facet_grid(as.factor(Mouse) ~ .)  -> A
+pellet_intake %>%
+	filter(hour_bin != 12, hour_bin != 13) %>%
+	ggplot(aes(hour_bin, pellet_mean, ymin = pellet_mean - pellet_sem,
+		   ymax = pellet_mean + pellet_sem)) +
+	geom_line() +
+	geom_errorbar() +
+	scale_x_continuous(breaks = seq(0, 24)) +
+	theme_pubr() +
+	xlab("") +
+	theme(axis.text.x = element_blank()) -> B
+grid.arrange(B, A, nrow = 2) -> intake
+ggsave("intake.png", intake)
+
 
 pellets_count_day %>%
 	ggplot(aes(lubridate::hour(date), removed, group = Mouse)) +
